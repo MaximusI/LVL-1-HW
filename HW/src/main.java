19b782ac76e9ecc7264f794671064f15d140b25e
Maxim <maks.voley@gmail.com>
2017-06-12 00:01:51 +0300

    Initialize variables

--- HW/src/main.java
+++ HW/src/main.java
@@ -3,6 +3,16 @@
  */
 public class main {
     public static void main(String[] args) {
+        byte bt = 1;
+        short sh = 2;
+        int it = 3;
+        long ln = 4;
+        float fl = 4;
+        double db = 3f/5;
+        boolean bl = false;
+        char ch = 'A';
         
+
+
     }
 }
