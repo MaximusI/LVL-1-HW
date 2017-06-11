590039c1160ca80a4c691e6331c121c3c8062644
Maxim <maks.voley@gmail.com>
2017-06-12 00:55:35 +0300

    Add method leapYear

--- HW/src/main.java
+++ HW/src/main.java
@@ -11,7 +11,13 @@ public class main {
         double db = 3f/5;
         boolean bl = false;
         char ch = 'A';
-        System.out.println(dz(bt, sh, it, ch));
+//        *** test code ***
+//        System.out.println(dz(bt, sh, it, ch));
+//        System.out.println(leapYear(1700));
+//        System.out.println(leapYear(1800));
+//        System.out.println(leapYear(1900));
+//        System.out.println(leapYear(1600));
+//        System.out.println(leapYear(2004));
     }
 
     public static double dz(int a, int b, int c, int d) {
@@ -22,5 +28,16 @@ public class main {
         if ((a + b >= 10) && (a + b <= 20))
             return true;
         return false;
+    }
+
+    /** Написать метод, который определяет является ли год високосным. Каждый 4-й год является
+    високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+     */
+    public static boolean leapYear(int year) {
+        if (year/100 % 4 ==0 &&  year % 4 == 0)
+            return true;
+        else if (year % 100 == 0)
+            return false;
+        else return false;
     }
 }
